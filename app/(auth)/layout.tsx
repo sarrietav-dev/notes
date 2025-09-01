74b9cf9305d45050b24ad8cc8aad0dd9fc2a2579
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2025-09-01 18:24:47 -0500

    Add fonts, images, and segment the routes into auth and app

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter, Noto_Serif, Source_Code_Pro } from "next/font/google";
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const notoSerif = Noto_Serif({
+  subsets: ["latin"],
+  variable: "--font-noto-serif",
+});
+const sourceCodePro = Source_Code_Pro({
+  subsets: ["latin"],
+  variable: "--font-source-code-pro",
+});
+
+export const metadata: Metadata = {
+  title: "Notes",
+  description: "Notetaking app!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${notoSerif.variable} ${sourceCodePro.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
